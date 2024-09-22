587a3baa201fd4e6ec03faee2ee16fd66d7576b7
lucaswyd <99453165+lucaswyd@users.noreply.github.com>
2024-09-22 18:18:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -369,23 +369,23 @@ nflBye[14] = [
 // Define the start date of the NFL season
 const startDate = new Date('2024-09-05'); // NFL season starts on September 5, 2024
 const byeWeeks = {
-    1: "sports/week1.html",
-    2: "sports/week2.html",
-    3: "sports/week3.html",
-    4: "sports/week4.html",
-    5: "sports/week5.html",
-    6: "sports/week6.html",
-    7: "sports/week7.html",
-    8: "sports/week8.html",
-    9: "sports/week9.html",
-    10: "sports/week10.html",
-    11: "sports/week11.html",
-    12: "sports/week12.html",
-    13: "sports/week13.html",
-    14: "sports/week14.html",
-    15: "sports/week15.html",
-    16: "sports/week16.html",
-    17: "sports/week17.html"
+    1: "weeks/week1.html",
+    2: "weeks/week2.html",
+    3: "weeks/week3.html",
+    4: "weeks/week4.html",
+    5: "weeks/week5.html",
+    6: "weeks/week6.html",
+    7: "weeks/week7.html",
+    8: "weeks/week8.html",
+    9: "weeks/week9.html",
+    10: "weeks/week10.html",
+    11: "weeks/week11.html",
+    12: "weeks/week12.html",
+    13: "weeks/week13.html",
+    14: "weeks/week14.html",
+    15: "weeks/week15.html",
+    16: "weeks/week16.html",
+    17: "weeks/week17.html"
 };
 
 function getCurrentWeek() {
